990f09f575cb6757497f1488a3222f577f2150aa
Bhuwan Prasad Upadhyay <40879634+bhuwanupadhyay@users.noreply.github.com>
2023-02-25 17:26:17 -0600

    Add keycloak chart (#48)
    * chore: setup project
    
    * chore: setup project
    
    * chore: setup project
    
    * fix: common chart setup
    
    * fix: common chart setup
    
    * fix: common chart setup
    
    * fix: common chart setup
    
    * fix: common chart setup
    
    * ci: fix taskfile tools.yaml
    
    * add keycloak chart
    
    * add keycloak chart
    
    * add keycloak chart
    
    * add keycloak chart
    
    * add keycloak chart
    
    * add keycloak chart
    
    * add keycloak chart
    
    * add keycloak chart

---
+++ k8s/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+# create self-signed certificate
+DOMAIN=k8s.localdev
+
+if test -f "$DOMAIN+1-key.pem"; then
+  echo "File $DOMAIN+1-key.pem already exists"
+else
+  echo "File $DOMAIN+1-key.pem does not exist"
+  mkcert $DOMAIN "*.$DOMAIN"
+fi
+
+# create ingress-certs secret
+SECRET_NAME=ingress-certs
+
+for ns in kube-addons keycloak; do
+
+  # create namespace if not exists
+  if kubectl get namespace $ns >/dev/null 2>&1; then
+    echo "Namespace $ns already exists"
+  else
+    kubectl create namespace $ns
+  fi
+
+  kubectl create secret tls $SECRET_NAME \
+    --key $DOMAIN+1-key.pem \
+    --cert $DOMAIN+1.pem \
+    --namespace $ns \
+    --dry-run=client \
+    -o yaml | kubectl apply -f -
+
+done
